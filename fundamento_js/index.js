27b8b8b9147af16f9dc2dd708dece65386c0fd79
JuanDiego <arenas250421@gmail.com>
2023-03-14 11:33:52 -0500

    pendiente

--- fundamento_js/index.js
+++ fundamento_js/index.js
@@ -1,68 +1,140 @@
-// // ---------------------------------------------------------
+// ---------------------------------------------------------
 
-// function suma(num1, num2) {
-// 	return console.log('La suma de', num1, 'y', num2, 'es:', num1 + num2);
-// }
+function suma1(num1, num2) {
+	return console.log('La suma de', num1, 'y', num2, 'es:', num1 + num2);
+}
 
-// suma(5, 6);
+suma1(5, 6);
 
-// //---------------------------------------------------------
+//---------------------------------------------------------
 
-// function str (strings) {
-// 	return console.log(strings.length);
-// }
+function str2 (strings2) {
+	return console.log(strings2.length);
+}
 
-// str('Hola')
+str2('Hola')
 
-// // ---------------------------------------------------------
+// ---------------------------------------------------------
 
-// function maxNumber(arr) {
-// 	return console.log(Math.max(...arr));
-// }
+function maxNumber3(arr3) {
+	return console.log(Math.max(...arr3));
+}
 
-// let array = [0, 2, 4, 5, 1, 3];
+let array3 = [0, 2, 4, 5, 1, 3];
 
-// maxNumber(array);
+maxNumber3(array3);
 
-// function maxNum(arr) {
-// 	let max = arr[0];
-// 	for (let i = 1; i < arr.length; i++) {
-// 		if (arr[i] > max) {
-// 			max = arr[i];
-// 		}
-// 	}
-// 	return max;
-// }
+function maxNum3(arr3) {
+	let max3 = arr3[0];
+	for (let i = 1; i < arr3.length; i++) {
+		if (arr3[i] > max3) {
+			max3 = arr3[i];
+		}
+	}
+	return max3;
+}
 
-// const array = [4, 10000];
-// console.log(maxNum(array));
-
-// // ---------------------------------------------------------
-
-// function upper(str) {
-// 	return str.toUpperCase();
-// }
-
-// console.log(upper('hola'));
+array3 = [4, 10000];
+console.log(maxNum3(array3));
 
 // // ---------------------------------------------------------
 
-// function array(objet) {
-// 	return Object.keys(objet);
-// }
+function upper4(str4) {
+	return str4.toUpperCase();
+}
 
-// let objeto = {
-// 	nombre: 'Juan',
-// 	edad: 20,
-// };
-
-// console.log(array(objeto));
+console.log(upper4('hola'));
 
 // // ---------------------------------------------------------
 
-function order(arr) {
-	return arr.sort((a, b) => a - b);
+function array5(objet5) {
+	return Object.keys(objet5);
+}
+
+let objeto5 = {
+	nombre: 'Juan',
+	edad: 20,
 };
 
-let nums = [7, 2, 10, 1, 5, 6, 3, 4, 11, 9, 8];
-console.log(order(nums));
+console.log(array5(objeto5));
+
+// // ---------------------------------------------------------
+
+function order(arr6) {
+	return arr6.sort((a, b) => a - b);
+};
+
+let num6 = [7, 2, 10, 1, 5, 6, 3, 4, 11, 9, 8];
+console.log(order(num6));
+
+// // ---------------------------------------------------------
+
+function days7 (date71, date72) {
+	const diff7 = new Date(date72).getTime() - new Date(date71).getTime();
+	
+	return diff7 / (1000 * 60 * 60 * 24);
+};
+
+console.log(days7('2023-03-14', '2023-03-15'));
+
+// // ---------------------------------------------------------
+
+function length8 (arr8) {
+	return arr8.map(str => str.length);
+}
+
+let arr8 = ['Yo', 'estudio', 'ADSO', 'en', 'el', 'SENA']
+console.log(length8(arr8));
+console.log(arr8);
+
+// // ---------------------------------------------------------
+
+// let objet9 = {
+// 	nombre: 'Juan',
+// 	edad: '20',
+// }
+
+// function invertir (objet9) {
+// 	const miObjet = {
+// 		nombre: objet9.nombre.split("").reverse().join(""),
+// 		edad: objet9.edad.split("").reverse().join("")
+// 	}
+// 	return miObjet
+// }
+
+// console.log(invertir(objet9))
+
+// console.log(Object.keys);
+
+// // ---------------------------------------------------------
+
+function par10 (num10) {
+	for (let i = 0; i < num10.length; i++) {
+		const element = num10[i];
+		return element;
+	}
+}
+
+console.log(par10(8));
+
+
+function arrayNum(maxNum){
+	let miarray = []
+
+	for (let i = 1; i <= maxNum; i++){
+		miarray.push(i)
+	}
+	return miarray
+}
+
+console.log(arrayNum(5))
+
+// // ---------------------------------------------------------
+
+function upper11 (arr11) {
+	return arr11.map(str => str.length);
+}
+
+let text = ['Yo', 'estudio', 'ADSO', 'en', 'el', 'SENA']
+console.log(length8(text));
+console.log(arr8);
--- fundamento_js/practica.js
+++ fundamento_js/practica.js
@@ -1,39 +1,44 @@
-// 4. Bucles
-for (let i = 0; i <= 1; i++) {
-	console.log(i);
-}
+// // 4. Bucles
+// for (let i = 0; i <= 1; i++) {
+// 	console.log(i);
+// }
 
-// 5. Function
-function suma(num1, num2) {
-	return console.log('la suma de', num1, 'y', num2, 'es:', num1 + num2);
-}
+// // 5. Function
+// function suma(num1, num2) {
+// 	return console.log('la suma de', num1, 'y', num2, 'es:', num1 + num2);
+// }
 
-suma(5, 7);
+// suma(5, 7);
 
-// 6. Arrays
-let numbers = [1, 2, 3, 4];
-for (let i = 0; i < numbers.length; i++) {
-	console.log(numbers[i]);
-}
+// // 6. Arrays
+// let numbers = [1, 2, 3, 4];
+// for (let i = 0; i < numbers.length; i++) {
+// 	console.log(numbers[i]);
+// }
 
-// 7. Objet
-let people = {
-	name: 'Juan Diego',
-	age: '20',
-	genero: 'man',
-};
+// // 7. Objet
+// let people = {
+// 	name: 'Juan Diego',
+// 	age: '20',
+// 	genero: 'man',
+// };
 
-console.log(people.name, people.age);
-console.log(people);
+// console.log(people.name, people.age);
+// console.log(people);
 
-// ? 8. Arrays methods pendiente
-let number = [1, 2, 3];
-number.forEach(function (numero) {
-	console.log(numero);
-});
+// // ? 8. Arrays methods pendiente
+// let number = [1, 2, 3];
+// number.forEach(function (numero) {
+// 	console.log(numero);
+// });
 
-// 10. Events
-let button = document.getElementById('buttons');
-button.addEventListener('click', function () {
-	console.log('Se hizo click en el botón');
-});
+// // 10. Events
+// let button = document.getElementById('buttons');
+// button.addEventListener('click', function () {
+// 	console.log('Se hizo click en el botón');
+// });
+let nombre = 'Juan Diego';
+let edad = 20;
+
+console.log(nombre, 'tine', edad, 'años');
+

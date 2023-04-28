3701207a02469f96bc55a92ad178f87e01735b4a
Juan Diego <arenas250421@gmail.com>
2023-04-28 11:01:58 -0500

    cambios

--- ejercicioPractico/cambiarTexto/js/script.js
+++ ejercicioPractico/cambiarTexto/js/script.js
@@ -6,16 +6,16 @@ const texto = document.getElementsByTagName('h3');
 
 showTextButton.addEventListener('mousedown', () => {
 	const text = input.value;
-    if (text == '') {
-        errorText.style.display = 'block';
-        showTextButton.style.display = 'none'
-        setTimeout(() => {
-            errorText.style.display = 'none';
-            showTextButton.style.display = 'block'
-        }, 4000);
-    } else {
-        output.textContent = text;
-        texto[0].style.display = 'block'
-    }
-    console.log(output);
+	if (text == '') {
+		showTextButton.style.display = 'none';
+		errorText.style.display = 'block';
+		setTimeout(() => {
+			errorText.style.display = 'none';
+			showTextButton.style.display = 'block';
+		}, 4000);
+	} else {
+		output.textContent = text;
+		texto[0].style.display = 'block';
+	}
+	console.log(output);
 });

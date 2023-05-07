1fd86b2094925c258efbffcc431dd740f2056357
Juan Diego wsl <Arenas250421@gmail.com>
2023-05-07 14:28:11 -0500

    carrusel

---
+++ carrusel-004/js/script.js
@@ -0,0 +1,38 @@
+const btnLeft = document.querySelector('.btn-left');
+const btnRight = document.querySelector('.btn-right');
+const botones = document.querySelector('.btn');
+const imagenes = document.querySelector('.images');
+
+let imgMuestra = ['001', '002', '003', '004', '005', '006'];
+
+let contador = 0;
+
+
+const derecha = () => {
+	contador++;
+	if (contador > imgMuestra.length - 1) {
+		contador = 0;
+        console.log(contador);
+	}
+	imagenes.style.background = `url(img/${imgMuestra[contador]}.jpg)`;
+    imagenes.style.backgroundPosition= 'center';
+    imagenes.style.backgroundRepeat= 'no-repeat';
+    imagenes.style.backgroundAttachment = 'fixed';
+    imagenes.style.backgroundSize = 'cover';
+    
+};
+btnRight.addEventListener('click', derecha);
+
+const izquierda = () => {
+	contador--;
+	if (contador < 0) {
+		contador = 5;
+	}
+	imagenes.style.background = `url(img/${imgMuestra[contador]}.jpg)`;    
+    imagenes.style.backgroundPosition = 'center';
+	imagenes.style.backgroundRepeat = 'no-repeat';
+	imagenes.style.backgroundAttachment = 'fixed';
+	imagenes.style.backgroundSize = 'cover';
+};
+btnLeft.addEventListener('click', izquierda);
+ 

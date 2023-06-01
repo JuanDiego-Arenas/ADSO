34aea4f0493a99be432a244ccdd67454d8d9e8aa
Juan Diego wsl <Arenas250421@gmail.com>
2023-05-31 22:31:26 -0500

    primera parte del script

--- api-009/js/script.js
+++ api-009/js/script.js
@@ -1,20 +1,43 @@
-const containerTxt = document.getElementsByClassName('container-palabra');
 const myInput = document.getElementById('input');
-const textoPrueba = document.getElementById('prueba');
+const esperaTxt = document.getElementById('texto');
+const result = document.getElementById('resultado');
+
+const getResultado = async () => {
+	let wordInp = myInput.value;
+
+	result.innerHTML = `
+	<h2 class="loanding">
+		Cargando Resultados...
+	</h2>`;
+	let fetchUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordInp}`;
+	let data = await fetch(fetchUrl).then(res => res.json());
+
+	
+	result.innerHTML = `
+	<div class="word">
+		<div class="text-word">
+			<h2>Word:  ${data[0].word}</h2>
+			<span>${data[0].meanings[0].partOfSpeech} /${data[0].phonetics[1].text}/</span>
+		</div>
+		<div id="sound">
+			<i class="fa-solid fa-volume-high"></i>
+		</div>
+	</div>
+	`;
+	
+	const audioEl = document.createElement('audio');
+	audioEl.src = `${data[0].phonetics[0].audio}`;
+	const btnAudio = document.getElementById('sound')
+	btnAudio.addEventListener('click', () => {
+		audioEl.play();
+		console.log('Hola');
+	});
+};
 
 myInput.addEventListener('keyup', event => {
 	if (event.key === 'Enter' && myInput.value !== '') {
-		textoPrueba.style.display = 'none';
-        // containerTxt.style.display = 'flex';
-
-		const palabra = document.createElement('h2');
-		palabra.classList.add('palabra');
-        containerTxt[0].appendChild(palabra);
-        palabra.innerText = `Word: ${null}`;
-	} else {
-        containerTxt.style.display = 'none';
-		textoPrueba.style.display = 'block';
-		textoPrueba.innerText =
-			"Digita una palabra y presiona 'Enter' para obtener los resultado";
+		esperaTxt.style.display = 'none';
+		// containerTxt.style.display = 'flex';
+		getResultado();
 	}
 });

ff7debdb6a101476b5715c16b0ea47d7ab153aac
Juan Diego wsl <Arenas250421@gmail.com>
2023-06-07 22:57:31 -0500

    pagina 23, laravel

--- crudLoginAgenda/routes/web.php
+++ crudLoginAgenda/routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
-
+Route::resource('contactos', App\Http\Controllers\ContactoController::class);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

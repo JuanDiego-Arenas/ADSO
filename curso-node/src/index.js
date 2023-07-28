ce499bb54bdfffbf2f2c509f918bd3c8762667fd
Juan Diego wsl <Arenas250421@gmail.com>
2023-07-28 10:35:31 -0500

    curso de node dia 2

--- curso-node/src/index.js
+++ curso-node/src/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(join(__dirname, "/views"));
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
 app.listen(3000);
-console.log('Server running on port', 3000);
+console.log("Server running on port", 3000);

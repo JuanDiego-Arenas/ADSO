b37390cbb864fc79dd19d4023ef0fa8a738c5aa3
Juan Diego wsl <Arenas250421@gmail.com>
2023-07-28 10:36:16 -0500

    Ejercicio pelicula pendiente

---
+++ dom-manipulation/pelicula/js/index.js
@@ -0,0 +1,40 @@
+const parrafo = document.querySelector('.datos');
+const actor = document.querySelector('.actor');
+const newElement = document.createElement('li');
+
+const mostrarDatos = () => {
+	const films = {
+		nombre: 'Oppenheimer',
+		actores: [
+			'Cillian Murphy: J. Robert Oppenheimer',
+			'Emily Blunt: Katherine "Kitty" Oppenheimer',
+			'Matt Damon: Gen. Leslie Groves Jr.',
+			'Robert Downey Jr.: Lewis Strauss',
+		],
+	};
+
+	console.log(films.actores[1]);
+	const actores = () => {
+		for (let i = 0; i < films.actores.length; i++) {
+			newElement.textContent = films.actores[i];
+			actor.appendChild(newElement);
+		}
+	};
+
+	return (parrafo.innerHTML = `
+        <h1>Pelicula: <strong>${films.nombre}</strong></h1>
+        <ul>
+
+            ${actores()}
+           
+        </ul>
+    `);
+};
+
+mostrarDatos();
+
+// ${films.actores.forEach(element => {
+//     console.log(element);
+// 	actor.appendChild(newE)
+//     newE.textContent = element
+// })}

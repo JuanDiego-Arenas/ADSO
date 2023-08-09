3c98bce135754e36c821129a38846c58ba923860
Juan Diego Arch <arenas250421@gmail.com>
2023-08-08 23:33:11 -0500

    prueba git

--- curso-node/src/index.js
+++ curso-node/src/index.js
@@ -11,7 +11,7 @@ console.log(join(__dirname, '/views'));
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(indexRoutes);
-app.use(express.static(join(__dirname, 'public')));
+// app.use(express.static(join(__dirname, 'public')));
 
 app.listen(3000);
 console.log('Server running on port', 3000);

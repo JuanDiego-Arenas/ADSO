fef0090a9528e5c08ce1ce704e248a48315f92fb
Juan Diego Arch <Arenas250421@gmail.com>
2023-08-16 10:33:44 -0500

    base teminada

--- curso-node/src/routes/index.routes.js
+++ curso-node/src/routes/index.routes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) =>
-	res.render('index', { title: 'Node.js Index--' })
+	res.render('index', { title: 'Node.js' })
 );
 router.get('/about', (req, res) =>
 	res.render('about', { title: 'Node.js About me' })

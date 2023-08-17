8af9e3a820ad33b93c0795514b132d2d25b54751
Juan Diego Arch <Arenas250421@gmail.com>
2023-08-16 23:40:40 -0500

    About, servicios

--- curso-node/src/routes/index.routes.js
+++ curso-node/src/routes/index.routes.js
@@ -4,11 +4,14 @@ const router = Router();
 router.get('/', (req, res) =>
 	res.render('index', { title: 'InnovateTech.js' })
 );
-router.get('/about', (req, res) =>
-	res.render('about', { title: 'About me' })
-);
+router.get('/about', (req, res) => res.render('about', { title: 'About' }));
 router.get('/contact', (req, res) =>
-	res.render('contact', { title: 'Contact me' })
+	res.render('contact', { title: 'Contact' })
+);
+router.get('/mision', (req, res) => res.render('mision', { title: 'Misión' }));
+router.get('/vision', (req, res) => res.render('vision', { title: 'Visión' }));
+router.get('/services', (req, res) =>
+	res.render('services', { title: 'Servicios' })
 );
 
 export default router;

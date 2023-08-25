81ff18786af253cb325f59f5c3c8a33b2eb02c7d
Juan Diego Arch <Arenas250421@gmail.com>
2023-08-24 20:38:14 -0500

    index crud v0.4

--- node/crud-mysql/src/index.js
+++ node/crud-mysql/src/index.js
@@ -3,11 +3,13 @@ import { pool } from './db.js'
 
 const app = express();
 
+app.get('/ping', (req, res) => res.send('Pong'));
+
 app.get('/employees', (req, res) => res.send('Obteniendo Empleados'));
 app.post('/employees', (req, res) => res.send('Creando Empleados'));
 app.patch('/employees', (req, res) => res.send('Actualizando Empleados'));
 app.delete('/employees', (req, res) => res.send('Eliminando Empleados'));
 
-app.listen(3000);
-console.log('Server running on port', 3000);
+app.listen(process.env.PORT || 3000);
+console.log('Server running on port', process.env.PORT || 3000);
 console.log('http://localhost:3000/');
